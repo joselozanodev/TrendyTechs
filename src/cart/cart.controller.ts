2cad08ec9ac81faedb13a6a8d925efbf215b3896
Jose Lozano <joselozano.dev@gmail.com>
2024-03-15 11:55:47 -0400

    Add emptyCartItems and getCartTotalPrice functions And add validations to addItemsToCart and updateItemQuantity

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -16,6 +16,11 @@ export class CartController {
         return this.cartService.getCartById(id)
     }
 
+    @Get('/price/:id')
+    getCartTotalPrice(@Param('id') id: string){
+        return this.cartService.getCartTotalPrice(id)
+    }
+
     @Post(':id')
     addItemsToCart(@Param('id') id: string, @Body() createCartItemDto: Prisma.CartItemCreateInput){
         return this.cartService.addItemsToCart(id, createCartItemDto)
@@ -26,10 +31,19 @@ export class CartController {
         return this.cartService.deleteItemFromCart(id, product)
     }
 
+    @Delete('/empty/:id')
+    emptyCartItems(@Param('id') id: string){
+        return this.cartService.emptyCartItems(id)
+    }
+
     @Patch(':id')
     updateCartItemQuantity(@Param('id') id: string, @Body() quantity: number, @Query('product') product: string){
         return this.cartService.updateCartItemQuantity(id, quantity, product)
     }
 
 
+
 }
+
+
+// Verifica que la cantidad de productos no sea negativa o excesivamente alta.
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -29,21 +29,39 @@ export class CartService {
     }
     
     async addItemsToCart(cartId: string, createItemDto: Prisma.CartItemCreateInput) {
+        try {
 
-        const cartItem = await this.databaseService.cartItem.create({
-            data: createItemDto,
-        });
+            const existingItem = await this.databaseService.cartItem.findFirst({
+                where: {
+                    cartId: cartId, 
+                    productId: createItemDto["productId"]
+                }
+            })
 
-        const cart = this.databaseService.cart.findUnique({
-            where: {
-                id: cartId
-            },
-            include: {
-                items: true
+            if(existingItem || createItemDto.quantity <= 0){
+                console.log("Product already exists or quantity isn't a correct value")
+                throw new NotFoundException("Product already exists or quantity isn't a correct value")
             }
-        })
 
-        return (await cart).items.find(item => item.id === cartItem.id)
+            const cartItem = await this.databaseService.cartItem.create({
+                data: createItemDto,
+            });
+    
+            const cart = this.databaseService.cart.findUnique({
+                where: {
+                    id: cartId
+                },
+                include: {
+                    items: true
+                }
+            })
+    
+            return (await cart).items.find(item => item.id === cartItem.id)
+            
+        } catch (error) {
+            throw new NotFoundException("An error occurred while processing the request. Please check the provided data and try again.")
+        }
+
     }
 
     async deleteItemFromCart(cartId: string, productId: string){
@@ -69,29 +87,90 @@ export class CartService {
     }
 
     async updateCartItemQuantity(cartId: string, quantityToAdd: number, productId: string){
+
+        
         const existingItem = await this.databaseService.cartItem.findFirst({
             where: {
                 cartId,
                 productId,
             }
         })
-
-        if(existingItem){
-            
-               await this.databaseService.cartItem.update({
-                    where: {
-                        id: existingItem.id
-                    },
-                    data: {
-                        quantity: quantityToAdd + existingItem.quantity
-                    }
-                })
- 
-                return existingItem
-            
-        }
-
         
+        
+        try {
+            const newQuantity = existingItem.quantity + quantityToAdd['quantity']
+
+            if(newQuantity < 0){
+                console.log("Quantity mus be greater than 0")
+                throw new Error("Quantity mus be greater than 0")
+            }else if(newQuantity === 0){
+                return await this.deleteItemFromCart(cartId, productId) 
+            }
+
+            await this.databaseService.cartItem.update({
+                     where: {
+                         id: existingItem.id
+                     },
+                     data: {
+                         quantity: newQuantity
+                     }
+                 })
+                 return existingItem
+        } catch (error) {
+            throw new NotFoundException('Item not found')
+        }
     }
 
+    async emptyCartItems(id: string){
+
+        try {
+            await this.databaseService.cartItem.deleteMany({
+                where: {
+                    cartId: id
+                }
+            })
+
+            return await this.databaseService.cart.findUnique({
+                where: {
+                    id,
+                },
+                include: {
+                    items: true
+                }
+            })
+            
+        } catch (error) {
+            throw new NotFoundException('Cart not found')
+        }
+    }
+
+    async getCartTotalPrice(id: string) {
+        try {
+            const cart = await this.databaseService.cart.findUnique({
+                where: {
+                    id,
+                },
+                include: {
+                    items: {
+                        include: {
+                            product: true,
+                        },
+                    },
+                },
+            });
+    
+            if (!cart) {
+                throw new NotFoundException('Carrito no encontrado');
+            }
+    
+            // Calcula el precio total sumando los precios de los productos multiplicados por sus cantidades
+            const totalPrice = cart.items.reduce((acc, item) => {
+                return acc + item.product.price * item.quantity;
+            }, 0);
+    
+            return {totalPrice: totalPrice}
+        } catch (error) {
+            throw new NotFoundException('Carrito no encontrado');
+        }
+    }
 }
